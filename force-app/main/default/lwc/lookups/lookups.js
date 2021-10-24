8b4d671e9edd1b9e3ce4d05cbea53a22a7dcf8d5
TsSlava <ts.slava@gmail.com>
2021-10-25 00:03:21 +0300

    First commit

---
+++ force-app/main/default/lwc/lookups/lookups.js
@@ -0,0 +1,12 @@
+import { LightningElement, wire, api } from 'lwc';
+import getObjectList from '@salesforce/apex/SearchObject.getObjectList';
+
+export default class Lookups extends LightningElement {
+
+    @api
+    sObjectName;
+
+    @wire(getObjectList, {sObjectName : '$sObjectName'})
+    accounts;
+    
+}
