8527371261a92303af8b5664954ec810089e276f
TsSlava <ts.slava@gmail.com>
2021-10-25 17:54:43 +0300

    Add Descending Order To Filtration

--- force-app/main/default/lwc/lookups/lookups.js
+++ force-app/main/default/lwc/lookups/lookups.js
@@ -8,13 +8,15 @@ export default class Lookups extends LightningElement {
 
     limit = '1';
 
+    order = 'ASC';
+
     @track
     boxClass = 'slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click';
 
     @track
     blurTimeout;
 
-    @wire(getObjectList, {sObjectName : '$sObjectName', limits : '$limit'})
+    @wire(getObjectList, {sObjectName : '$sObjectName', limits : '$limit', orderOfSearch : '$order'})
     objects;
 
     get options() {
@@ -22,12 +24,24 @@ export default class Lookups extends LightningElement {
             { label: '1', value: '1'},
             { label: '3', value: '3'},
             { label: '5', value: '5'},
-            { label: '10', value: '10'}
+            { label: '10', value: '10'},
+            { label: '100', value: '100'}
         ];
+    }
+
+    get optionsOfOrder() {
+        return [
+            { label: 'Ascending Order', value: 'ASC'},
+            { label: 'Descending Order', value: 'DESC'}
+        ]
     }
 
     handleChange(event) {
         this.limit = event.detail.value;
+    }
+
+    handleChangeOrder(event) {
+        this.order = event.detail.value;
     }
 
     handleClick() {
