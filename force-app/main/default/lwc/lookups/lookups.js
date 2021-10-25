de17d78d3d08d79dc0276c7a87d2d793ea7ebc46
TsSlava <ts.slava@gmail.com>
2021-10-25 16:15:23 +0300

    Add dropdown list and blur

--- force-app/main/default/lwc/lookups/lookups.js
+++ force-app/main/default/lwc/lookups/lookups.js
@@ -1,4 +1,4 @@
-import { LightningElement, wire, api } from 'lwc';
+import { LightningElement, wire, api, track } from 'lwc';
 import getObjectList from '@salesforce/apex/SearchObject.getObjectList';
 
 export default class Lookups extends LightningElement {
@@ -6,28 +6,36 @@ export default class Lookups extends LightningElement {
     @api
     sObjectName;
 
-    // @api
-    // value;
-
     limit = '1';
+
+    @track
+    boxClass = 'slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click';
+
+    @track
+    blurTimeout;
 
     @wire(getObjectList, {sObjectName : '$sObjectName', limits : '$limit'})
     objects;
 
-    // handleChange(event) {
-    //     this.objects = event.detail.value;
-    // }
-
     get options() {
         return [
             { label: '1', value: '1'},
-            { label: '2', value: '2'},
-            { label: '3', value: '3'}
+            { label: '3', value: '3'},
+            { label: '5', value: '5'},
+            { label: '10', value: '10'}
         ];
     }
 
     handleChange(event) {
         this.limit = event.detail.value;
+    }
+
+    handleClick() {
+        this.boxClass = 'slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-has-focus slds-is-open';
+    }
+
+    onBlur() {
+        this.blurTimeout = setTimeout(() => {this.boxClass = 'slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-has-focus', 300});
     }
     
 }
