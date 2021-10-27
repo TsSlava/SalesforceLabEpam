cced433bdcfa5fa7d1e67f2783478030bd7c2906
TsSlava <ts.slava@gmail.com>
2021-10-27 11:43:13 +0300

    Add lookUp JS

--- force-app/main/default/lwc/lookUp/lookUp.js
+++ force-app/main/default/lwc/lookUp/lookUp.js
@@ -1,3 +1,35 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, api, track } from 'lwc';
+import getRecordList from '@salesforce/apex/LookUpController.getRecordList';
 
-export default class LookUp extends LightningElement {}
+export default class Lookups extends LightningElement {
+
+    @api
+    sObjectApiName;
+
+    addInfoFieldApiName = 'Phone';
+
+    optionsAddInfo = [
+        { label: 'Phone', value: 'Phone' },
+        { label: 'Industry', value: 'Industry' },
+        { label: 'City', value: 'BillingCity' }
+    ]
+
+    @track
+    boxClass = 'slds-combobox slds-m-left_small slds-m-right_small slds-dropdown-trigger slds-dropdown-trigger_click';
+
+    @wire(getRecordList, {sObjectName : '$sObjectApiName', addInfo : '$addInfoFieldApiName'})
+    records;
+
+    handleChangeAddInfo(event) {
+        this.addInfoFieldApiName = event.detail.value;
+    }
+
+    handleClick() {
+        this.boxClass = 'slds-combobox slds-m-left_small slds-m-right_small slds-dropdown-trigger slds-dropdown-trigger_click slds-has-focus slds-is-open';
+    }
+
+    handleBlur() {
+        this.boxClass = 'slds-combobox slds-m-left_small slds-m-right_small slds-dropdown-trigger slds-dropdown-trigger_click slds-has-focus';
+    }
+    
+}
